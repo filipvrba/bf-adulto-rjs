1e954ff33d472f67ff1e300da77a2d948a9d42ec
Filip Vrba <filipvrba@pm.me>
2023-10-29 12:43:44 +0100

    fix wget for encode chars

--- src/js/third-side/net.js
+++ src/js/third-side/net.js
@@ -1,6 +1,11 @@
 export default class Net {
   static wget(url, callback) {
-    return fetch(url).then(response => response.text()).then((text) => {
+    let headers = new Headers;
+    headers.append("Content-Type", "text/plain; charset=UTF-8");
+
+    return fetch(url, headers).then(response => response.arrayBuffer()).then((buffer) => {
+      let decoder = new TextDecoder("windows-1250");
+      let text = decoder.decode(buffer);
       if (callback) return callback(text)
     })
   }
--- src/rb/third-side/net.js.rb
+++ src/rb/third-side/net.js.rb
@@ -1,10 +1,16 @@
 export default class Net
   def self.wget url, &callback
-    fetch(url)
+    headers = Headers.new
+    headers.append('Content-Type','text/plain; charset=UTF-8')
+
+    fetch(url, headers)
     .then(lambda do |response|
-      response.text()
+      response.array_buffer()
     end)
-    .then(lambda do |text|
+    .then(lambda do |buffer|
+      decoder = TextDecoder.new('windows-1250')
+      text    = decoder.decode(buffer)
+
       callback(text) if callback
     end)
   end
